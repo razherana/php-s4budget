991f66ff807b3e6a1b078973f89050b868973be4
razherana <razherana@gmail.com>
2025-03-25 10:21:29 +0300

    Updated Database

--- bdd/base.sql
+++ bdd/base.sql
@@ -1,12 +1,13 @@
--- Active: 1738011862925@@127.0.0.1@3306@Gestion_budget
 CREATE DATABASE Gestion_budget;
-
 USE Gestion_budget;
 
 CREATE TABLE Departement (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50)
+    name VARCHAR(50),
+    icon VARCHAR(50)
 );
+
+-- DEPARTEMENT -> CATEGORIES : PPN -> TYPES : RIZ, ETC -> (PREVISIONS (DEPENSE | RECETTE) : LIBELLE (ANARANY), REALISATION)
 
 CREATE TABLE User (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -19,22 +20,33 @@ CREATE TABLE User (
 
 CREATE TABLE Budget (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    id_departement INT,
-    Solde DECIMAL (15, 2),
+    id_departement INT, 
+    solde DECIMAL (15, 2),
+    updated_at TIMESTAMP,
     FOREIGN KEY (id_departement) REFERENCES Departement (id)
+);
+
+CREATE TABLE Categorie (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    designation VARCHAR(50), -- PPN, ETC
+    id_departement INT,
+    FOREIGN KEY (id_departement) REFERENCES Departement (id)
+);
+
+CREATE TABLE Type (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    designation VARCHAR(50), -- RIZ, ETC
+    id_categorie INT,
+    FOREIGN KEY (id_categorie) REFERENCES Categorie (id)
 );
 
 CREATE TABLE Prevision (
     id INT PRIMARY KEY AUTO_INCREMENT,
     designation VARCHAR(50),
-    id_departement INT,
-    FOREIGN KEY (id_departement) REFERENCES Departement (id)
-);
-
-CREATE TABLE Realisation (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    designation VARCHAR(50),
-    type VARCHAR(50),
+    realisation DECIMAL (15, 2),
     prevision DECIMAL (15, 2),
-    solde DECIMAL (15, 2)
+    type INT(1), -- DEPENSE OU RECETTE
+    id_type INT,
+    `date` DATE DEFAULT CURRENT_DATE,
+    FOREIGN KEY (id_type) REFERENCES Type (id)
 );
